a958c7449c0cbbf9719c959943c103354395732b
RusticPrism <rusticprism2611@gmail.com>
2022-05-29 19:22:10 +0200

    1.0.0 Beta

--- src/main/java/de/rusticprism/kreisclient/mixin/HitboxMixin.java
+++ src/main/java/de/rusticprism/kreisclient/mixin/HitboxMixin.java
@@ -4,14 +4,17 @@ import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.entity.Entity;
 import net.minecraft.resource.SynchronousResourceReloader;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(EntityRenderDispatcher.class)
 public abstract class HitboxMixin implements SynchronousResourceReloader {
 
-    @Redirect(method = "render",at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;doesRenderOnFire()Z"))
+    @Shadow private boolean renderHitboxes;
+
+    @Redirect(method = "render",at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;isInvisible()Z"))
     public boolean render(Entity instance) {
-        return true;
+        return !renderHitboxes;
     }
 }
