db46b8f8fe694353c75ee7275e7733b081345676
RusticPrism <rusticprism2611@gmail.com>
2022-06-14 20:36:41 +0200

    1.0.0 Beta

--- src/main/java/de/rusticprism/kreisclient/mixin/MinecraftClientMixin.java
+++ src/main/java/de/rusticprism/kreisclient/mixin/MinecraftClientMixin.java
@@ -1,19 +1,63 @@
 package de.rusticprism.kreisclient.mixin;
 
 import de.rusticprism.kreisclient.KreisClient;
+import de.rusticprism.kreisclient.render.SplashOverlay;
+import net.minecraft.SharedConstants;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Overlay;
 import net.minecraft.client.option.NarratorMode;
+import net.minecraft.client.resource.ResourceReloadLogger;
+import net.minecraft.resource.ReloadableResourceManagerImpl;
+import net.minecraft.resource.ResourcePack;
+import net.minecraft.resource.ResourcePackManager;
+import net.minecraft.util.Unit;
+import net.minecraft.util.Util;
+import net.minecraft.util.thread.ReentrantThreadExecutor;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
 @Mixin(MinecraftClient.class)
-public class MinecraftClientMixin {
+public abstract class MinecraftClientMixin extends ReentrantThreadExecutor<Runnable> {
+
+    @Shadow @Final private ReloadableResourceManagerImpl resourceManager;
+
+    @Shadow @Final private static CompletableFuture<Unit> COMPLETED_UNIT_FUTURE;
+
+    @Shadow @Final private ResourcePackManager resourcePackManager;
+
+    @Shadow protected abstract void handleResourceReloadException(Throwable throwable);
+
+    @Shadow protected abstract void checkGameData();
+
+    @Shadow @Final private ResourceReloadLogger resourceReloadLogger;
+
+    public MinecraftClientMixin(String string) {
+        super(string);
+    }
+
     @Inject(method = "updateWindowTitle",at = @At("HEAD"),cancellable = true)
     public void setWindowTitle(CallbackInfo ci) {
         ci.cancel();
         MinecraftClient.getInstance().getWindow().setTitle("KreisClient " + KreisClient.version + "  Minecraft: 1.18.2");
         MinecraftClient.getInstance().options.getNarrator().setValue(NarratorMode.OFF);
+    }
+    @ModifyArg(method = "<init>",at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;setOverlay(Lnet/minecraft/client/gui/screen/Overlay;)V"),index = 0)
+    private Overlay setOverlay(Overlay overlay) {
+        List<ResourcePack> list = this.resourcePackManager.createResourcePacks();
+        return new SplashOverlay(MinecraftClient.getInstance(), this.resourceManager.reload(Util.getMainWorkerExecutor(), this, COMPLETED_UNIT_FUTURE, list), (throwable) -> Util.ifPresentOrElse(throwable, this::handleResourceReloadException, () -> {
+            if (SharedConstants.isDevelopment) {
+                this.checkGameData();
+            }
+
+            this.resourceReloadLogger.finish();
+        }), false);
     }
 }
---
+++ src/main/java/de/rusticprism/kreisclient/render/SplashOverlay.java
@@ -0,0 +1,181 @@
+package de.rusticprism.kreisclient.render;
+
+import com.mojang.blaze3d.platform.GlStateManager;
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Overlay;
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.resource.metadata.TextureResourceMetadata;
+import net.minecraft.client.texture.NativeImage;
+import net.minecraft.client.texture.ResourceTexture;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.resource.DefaultResourcePack;
+import net.minecraft.resource.ResourceManager;
+import net.minecraft.resource.ResourceReload;
+import net.minecraft.resource.ResourceType;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.Util;
+import net.minecraft.util.math.ColorHelper;
+import net.minecraft.util.math.MathHelper;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.IntSupplier;
+
+public class SplashOverlay extends Overlay{
+    static final Identifier LOGO = new Identifier("kreisclient:backgrounds/splash.png");
+    private static final int MOJANG_RED = ColorHelper.Argb.getArgb(255, 239, 50, 61);
+    private static final int MONOCHROME_BLACK = ColorHelper.Argb.getArgb(255, 0, 0, 0);
+    private static final IntSupplier BRAND_ARGB = () -> {
+        return (Boolean) MinecraftClient.getInstance().options.getMonochromeLogo().getValue() ? MONOCHROME_BLACK : MOJANG_RED;
+    };
+    private final MinecraftClient client;
+    private final ResourceReload reload;
+    private final Consumer<Optional<Throwable>> exceptionHandler;
+    private final boolean reloading;
+    private float progress;
+    private long reloadCompleteTime = -1L;
+    private long reloadStartTime = -1L;
+
+    public SplashOverlay(MinecraftClient client, ResourceReload monitor, Consumer<Optional<Throwable>> exceptionHandler, boolean reloading) {
+        this.client = client;
+        this.reload = monitor;
+        this.exceptionHandler = exceptionHandler;
+        this.reloading = reloading;
+    }
+
+    public static void init(MinecraftClient client) {
+        client.getTextureManager().registerTexture(LOGO, new SplashOverlay.LogoTexture());
+    }
+
+    private static int withAlpha(int color, int alpha) {
+        return color & 16777215 | alpha << 24;
+    }
+
+    public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+        int i = this.client.getWindow().getScaledWidth();
+        int j = this.client.getWindow().getScaledHeight();
+        long l = Util.getMeasuringTimeMs();
+        if (this.reloading && this.reloadStartTime == -1L) {
+            this.reloadStartTime = l;
+        }
+
+        float f = this.reloadCompleteTime > -1L ? (float)(l - this.reloadCompleteTime) / 1000.0F : -1.0F;
+        float g = this.reloadStartTime > -1L ? (float)(l - this.reloadStartTime) / 500.0F : -1.0F;
+        int k;
+        if (f >= 1.0F) {
+            if (this.client.currentScreen != null) {
+                this.client.currentScreen.render(matrices, 0, 0, delta);
+                RenderSystem.setShaderTexture(0, LOGO);
+                RenderSystem.setShaderColor(1.0F,1.0F,1.0F,1.0F);
+                RenderSystem.setShader(GameRenderer::getPositionTexShader);
+                drawTexture(matrices, 0, 0, this.client.getWindow().getScaledWidth(), this.client.getWindow().getScaledHeight(), 0.0F, 0.0F, 16, 128, 16, 128);
+            }
+
+            k = MathHelper.ceil((1.0F - MathHelper.clamp(f - 1.0F, 0.0F, 1.0F)) * 255.0F);
+            fill(matrices, 0, 0, i, j, withAlpha(BRAND_ARGB.getAsInt(), k));
+        } else if (this.reloading) {
+            if (this.client.currentScreen != null && g < 1.0F) {
+                this.client.currentScreen.render(matrices, mouseX, mouseY, delta);
+            }
+
+            k = MathHelper.ceil(MathHelper.clamp(g, 0.15, 1.0) * 255.0);
+            fill(matrices, 0, 0, i, j, withAlpha(BRAND_ARGB.getAsInt(), k));
+        } else {
+            k = BRAND_ARGB.getAsInt();
+            float m = (float)(k >> 16 & 255) / 255.0F;
+            float n = (float)(k >> 8 & 255) / 255.0F;
+            float o = (float)(k & 255) / 255.0F;
+            GlStateManager._clearColor(m, n, o, 1.0F);
+            GlStateManager._clear(16384, MinecraftClient.IS_SYSTEM_MAC);
+        }
+        double d = Math.min((double)this.client.getWindow().getScaledWidth() * 0.75, (double)this.client.getWindow().getScaledHeight()) * 0.25;
+        double e = d * 4.0;
+        int r = (int)(e * 0.5);
+        RenderSystem.setShaderTexture(0, LOGO);
+        RenderSystem.setShaderColor(1.0F,1.0F,1.0F,1.0F);
+        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+        drawTexture(matrices, 0, 0, this.client.getWindow().getScaledWidth(), this.client.getWindow().getScaledHeight(), 0.0F, 0.0F, 16, 128, 16, 128);
+        int s = (int)((double)this.client.getWindow().getScaledHeight() * 0.8325);
+        float t = this.reload.getProgress();
+        this.progress = MathHelper.clamp(this.progress * 0.95F + t * 0.050000012F, 0.0F, 1.0F);
+        if (f < 1.0F) {
+            this.renderProgressBar(matrices, i / 2 - r, s - 5, i / 2 + r, s + 5, 1.0F - MathHelper.clamp(f, 0.0F, 1.0F));
+        }
+
+        if (f >= 2.0F) {
+            this.client.setOverlay(null);
+        }
+
+        if (this.reloadCompleteTime == -1L && this.reload.isComplete() && (!this.reloading || g >= 2.0F)) {
+            try {
+                this.reload.throwException();
+                this.exceptionHandler.accept(Optional.empty());
+            } catch (Throwable var23) {
+                this.exceptionHandler.accept(Optional.of(var23));
+            }
+
+            this.reloadCompleteTime = Util.getMeasuringTimeMs();
+            if (this.client.currentScreen != null) {
+                this.client.currentScreen.init(this.client, this.client.getWindow().getScaledWidth(), this.client.getWindow().getScaledHeight());
+            }
+        }
+
+    }
+
+    private void renderProgressBar(MatrixStack matrices, int minX, int minY, int maxX, int maxY, float opacity) {
+        int i = MathHelper.ceil((float)(maxX - minX - 2) * this.progress);
+        int j = Math.round(opacity * 255.0F);
+        int k = ColorHelper.Argb.getArgb(j, 255, 255, 255);
+        fill(matrices, minX + 2, minY + 2, minX + i, maxY - 2, k);
+        fill(matrices, minX + 1, minY, maxX - 1, minY + 1, k);
+        fill(matrices, minX + 1, maxY, maxX - 1, maxY - 1, k);
+        fill(matrices, minX, minY, minX + 1, maxY, k);
+        fill(matrices, maxX, minY, maxX - 1, maxY, k);
+    }
+
+    public boolean pausesGame() {
+        return true;
+    }
+
+    @Environment(EnvType.CLIENT)
+    private static class LogoTexture extends ResourceTexture {
+        public LogoTexture() {
+            super(SplashOverlay.LOGO);
+        }
+
+        protected ResourceTexture.TextureData loadTextureData(ResourceManager resourceManager) {
+            MinecraftClient minecraftClient = MinecraftClient.getInstance();
+            DefaultResourcePack defaultResourcePack = minecraftClient.getResourcePackProvider().getPack();
+
+            try {
+                InputStream inputStream = defaultResourcePack.open(ResourceType.CLIENT_RESOURCES, SplashOverlay.LOGO);
+
+                ResourceTexture.TextureData var5;
+                try {
+                    var5 = new ResourceTexture.TextureData(new TextureResourceMetadata(true, true), NativeImage.read(inputStream));
+                } catch (Throwable var8) {
+                    if (inputStream != null) {
+                        try {
+                            inputStream.close();
+                        } catch (Throwable var7) {
+                            var8.addSuppressed(var7);
+                        }
+                    }
+
+                    throw var8;
+                }
+
+                inputStream.close();
+
+                return var5;
+            } catch (IOException var9) {
+                return new ResourceTexture.TextureData(var9);
+            }
+        }
+    }
+}
